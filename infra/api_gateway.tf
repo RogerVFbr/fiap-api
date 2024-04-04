d4239e06afb54d51db393b1d94629763e02b79c9
RogerVFbr <roger.freret@al.infnet.edu.br>
2024-04-04 19:58:34 -0300

    Initial configs

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -1,84 +1,100 @@
-resource "aws_apigatewayv2_api" "lambda" {
-  name          = "fiap-api-gw-${var.environment}"
-  protocol_type = "HTTP"
+locals {
+  api_name = "fiap-gw-${var.environment}"
 }
 
-resource "aws_cloudwatch_log_group" "api_gw" {
-  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
-  retention_in_days = 7
+resource "aws_api_gateway_rest_api" "api" {
+  name                     = local.api_name
+  description              = "API for ${local.api_name}"
+  binary_media_types       = ["*/*"]
+  minimum_compression_size = 0
 }
 
-resource "aws_apigatewayv2_stage" "lambda" {
-  api_id = aws_apigatewayv2_api.lambda.id
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "{proxy+}"
+}
 
-  name        = var.environment
-  auto_deploy = true
+resource "aws_api_gateway_method" "request_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
 
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+resource "aws_api_gateway_integration" "request_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_method.request_method.resource_id
+  http_method = aws_api_gateway_method.request_method.http_method
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.fiap_api.invoke_arn
+  integration_http_method = "POST"
+}
 
-    format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
-      httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
-      }
-    )
+# OPTIONS are needed for COORS
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "options_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
   }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"      = true
+    "method.response.header.Access-Control-Allow-Headers"     = true
+    "method.response.header.Access-Control-Allow-Methods"     = true
+    "method.response.header.Access-Control-Allow-Credentials" = true
+  }
+
+  depends_on = [aws_api_gateway_method.options_method]
 }
 
-#resource "aws_apigatewayv2_integration" "hello_world" {
-#  api_id = aws_apigatewayv2_api.lambda.id
-#  integration_uri    = aws_lambda_function.fiap_api.invoke_arn
-#  integration_type   = "AWS_PROXY"
-#  integration_method = "POST"
-#}
-#
-#resource "aws_apigatewayv2_route" "hello_world" {
-#  api_id = aws_apigatewayv2_api.lambda.id
-#
-#  route_key = "GET /hello"
-#  target    = "integrations/${aws_apigatewayv2_integration.hello_world.id}"
-#}
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.options_method.http_method
 
+  type             = "MOCK"
+  content_handling = "CONVERT_TO_TEXT"
 
-
-
-
-#resource "aws_apigatewayv2_integration" "lambda" {
-#  api_id           = aws_apigatewayv2_api.lambda.id
-#  integration_type = "HTTP_PROXY"
-#  integration_method = "ANY"
-##  integration_uri    = aws_lambda_function.fiap_api.invoke_arn
-#  integration_uri    = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.fiap_api.arn}/invocations"
-#}
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/apigatewayv2_route
-
-resource "aws_apigatewayv2_integration" "lambda" {
-  api_id           = aws_apigatewayv2_api.lambda.id
-  integration_type = "AWS_PROXY"
-  integration_method = "POST"
-  integration_uri    = aws_lambda_function.fiap_api.invoke_arn
-#  integration_uri    = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.fiap_api.arn}/invocations"
+  depends_on = [aws_api_gateway_method.options_method]
 }
 
-resource "aws_apigatewayv2_route" "example" {
-  api_id    = aws_apigatewayv2_api.lambda.id
-  route_key = "ANY /{proxy+}"
-  target = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = aws_api_gateway_method_response.options_200.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,DELETE,GET,HEAD,PATCH,POST,PUT'"
+  }
+
+  depends_on = [
+    aws_api_gateway_method_response.options_200,
+    aws_api_gateway_integration.options_integration,
+  ]
 }
 
+# Deployment
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = var.environment
 
-
-
-
-
-
-
+  depends_on = [
+    aws_api_gateway_integration.options_integration,
+    aws_api_gateway_integration.request_integration,
+  ]
+}
