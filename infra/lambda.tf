762636dcaa7d1b1b6d3e90754c5a4c6721145927
RogerVFbr <roger.freret@al.infnet.edu.br>
2024-04-04 21:25:52 -0300

    Initial configs

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -31,14 +31,6 @@ resource "aws_lambda_function" "fiap_api" {
   }
 }
 
-#resource "aws_lambda_permission" "api_gw" {
-#  statement_id  = "AllowExecutionFromAPIGateway"
-#  action        = "lambda:InvokeFunction"
-#  function_name = aws_lambda_function.fiap_api.function_name
-#  principal     = "apigateway.amazonaws.com"
-#
-#  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
-#}
 
 # API Permissions
 resource "aws_lambda_permission" "allow_api_gateway" {
