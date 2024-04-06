e4ce34878de77a9b16a1a6473262417fe9370b5f
RogerVFbr <roger.freret@al.infnet.edu.br>
2024-04-06 11:31:43 -0300

    Implementar cognito

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -111,10 +111,3 @@ resource "aws_api_gateway_deployment" "deployment" {
     aws_api_gateway_integration.request_integration,
   ]
 }
-
-resource "aws_api_gateway_authorizer" "api_authorizer" {
-  name          = "CognitoUserPoolAuthorizer"
-  type          = "COGNITO_USER_POOLS"
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  provider_arns = [aws_cognito_user_pool.this.arn]
-}
