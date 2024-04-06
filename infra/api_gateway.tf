5655f20cae2586aa99ea3255a8b2f2ad68269eea
RogerVFbr <roger.freret@al.infnet.edu.br>
2024-04-06 09:19:52 -0300

    Implementar cognito

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -19,15 +19,18 @@ resource "aws_api_gateway_method" "request_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
-  authorization = "NONE"
+
+#  authorization = "NONE"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.api_authorizer.id
 }
 
 resource "aws_api_gateway_integration" "request_integration" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_method.request_method.resource_id
-  http_method = aws_api_gateway_method.request_method.http_method
-  type        = "AWS_PROXY"
-  uri         = aws_lambda_function.fiap_api.invoke_arn
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_method.request_method.resource_id
+  http_method             = aws_api_gateway_method.request_method.http_method
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.fiap_api.invoke_arn
   integration_http_method = "POST"
 }
 
@@ -96,4 +99,11 @@ resource "aws_api_gateway_deployment" "deployment" {
     aws_api_gateway_integration.options_integration,
     aws_api_gateway_integration.request_integration,
   ]
+}
+
+resource "aws_api_gateway_authorizer" "api_authorizer" {
+  name          = "CognitoUserPoolAuthorizer"
+  type          = "COGNITO_USER_POOLS"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  provider_arns = [aws_cognito_user_pool.this.arn]
 }
