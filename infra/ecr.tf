fd1ec9204ae363da747a848a30b35172deb310dc
RogerVFbr <roger.freret@al.infnet.edu.br>
2024-11-22 09:49:22 -0300

    Redução imagem

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -57,7 +57,7 @@ resource "null_resource" "ecr_image" {
       echo "Building image ..."
       docker build -t ${aws_ecr_repository.repo.repository_url}:${local.ecr_image_tag} .
       echo "Slim build ..."
-      slim build --http-probe=false --include-workdir=true --include-path /usr/local --tag ${aws_ecr_repository.repo.repository_url}:${local.ecr_slim_image_tag} ${aws_ecr_repository.repo.repository_url}:${local.ecr_image_tag}
+      slim build --show-clogs --show-blogs --http-probe-cmd=GET:/warmup --include-workdir=true --include-path /usr/local --tag ${aws_ecr_repository.repo.repository_url}:${local.ecr_slim_image_tag} ${aws_ecr_repository.repo.repository_url}:${local.ecr_image_tag}
       docker image ls
       echo "Pushing image ..."
       docker push ${aws_ecr_repository.repo.repository_url}:${local.ecr_slim_image_tag}
