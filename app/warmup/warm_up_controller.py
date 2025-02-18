3a3c44591298ca529317b0b53e306102738f656a
rogerfreret <rogervf@gmail.com>
2025-02-18 15:48:58 -0300

    Improve logging

--- app/warmup/warm_up_controller.py
+++ app/warmup/warm_up_controller.py
@@ -4,10 +4,7 @@ from flask_smorest import Blueprint
 
 warm_up_controller = Blueprint("WarmUpController", __name__, description="Warm Up API")
 
-LOGGER = logging.getLogger(__name__)
-
 @warm_up_controller.route("/warmup")
 @warm_up_controller.response(200)
 def get_warmup():
-    LOGGER.info("Warm Up endpoint called.")
     return "Warm Up endpoint executed."
